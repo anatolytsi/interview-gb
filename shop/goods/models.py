cb6477fa1ad9ae74bba34ae3f5b15bc8521bf4d3
anatolytsi <tsirkunenkoat@gmail.com>
2022-05-29 13:06:34 +0200

    lesson4: added product model

--- shop/goods/models.py
+++ shop/goods/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, unique=True, blank=False, verbose_name=_('название'))
+    delivery_date = models.DateField(verbose_name=_('дата поставки'))
+    price = models.PositiveIntegerField(verbose_name=_('цена'))
+    units = models.CharField(max_length=5, blank=False, verbose_name=_('единица измерения'))
+    provider = models.CharField(max_length=50, verbose_name=_('поставщик'))
+
+    def __str__(self):
+        return self.name
