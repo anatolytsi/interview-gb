ed7c35c17fcbfa4a22d79abc4399d6994d3dbd5b
anatolytsi <tsirkunenkoat@gmail.com>
2022-05-29 19:00:54 +0200

    lesson5: added initial product categories

--- shop/goods/forms.py
+++ shop/goods/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Product
+from .models import Product, ProductCategory
 
 
 class DateInput(forms.DateInput):
@@ -16,7 +16,15 @@ class ProductForm(forms.ModelForm):
             'price',
             'units',
             'provider',
+            'categories',
         )
         widgets = {
             'delivery_date': DateInput()
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['categories'].widget = forms.widgets.CheckboxSelectMultiple()
+        self.fields['categories'].queryset = ProductCategory.objects.all()
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
--- shop/goods/management/commands/create_products.py
+++ shop/goods/management/commands/create_products.py
@@ -1,6 +1,8 @@
+import random
+
 from django.core.management.base import BaseCommand
 from mixer.backend.django import mixer
-from goods.models import Product
+from goods.models import Product, ProductCategory
 
 
 class Command(BaseCommand):
@@ -10,8 +12,12 @@ class Command(BaseCommand):
         parser.add_argument('amount', type=int)
 
     def handle(self, *args, **options):
+        categories = [cat for cat in ProductCategory.objects.all()]
         for i in range(options['amount']):
             name = f'Product {i}'
             if not Product.objects.filter(name=name).exists():
                 product = mixer.blend(Product, name=name)
+                max_cat_id = random.randint(0, len(categories))
+                for j in range(random.randint(0, max_cat_id), max_cat_id):
+                    product.categories.add(categories[j])
                 product.save()
