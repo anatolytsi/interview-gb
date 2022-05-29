8641b551d821267b714dbf81aa0b5f6d14421137
anatolytsi <tsirkunenkoat@gmail.com>
2022-05-29 14:40:35 +0200

    lesson4: added product list

--- shop/goods/urls.py
+++ shop/goods/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
+from .views import ProductList
 
+app_name = 'goods'
+
+urlpatterns = [
+    path('', ProductList.as_view(), name='product_list')
 ]
