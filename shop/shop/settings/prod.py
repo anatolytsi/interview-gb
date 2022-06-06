19af4b470c4dcd75bdaa1eabc1265a7d8d4953c3
anatolytsi <tsirkunenkoat@gmail.com>
2022-06-06 13:20:59 +0200

    lesson7: added postgre database support

--- shop/shop/settings/prod.py
+++ shop/shop/settings/prod.py
@@ -3,3 +3,16 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gb_shop',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
+
+STATIC_ROOT = BASE_DIR / '..' / 'nginx' / 'static'
